902b55a97dc469c3664128c929243851f49c3a06
Tam250399 <tamthanhsk25@gmail.com>
2021-01-07 16:57:17 +0700

    time

---
+++ introduction to java/SystemTime/src/App.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
